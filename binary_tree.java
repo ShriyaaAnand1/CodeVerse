053026b00f4e0bf77d1ff1a9f1451a6a5d941700
ShriyaaAnand1 <155803878+ShriyaaAnand1@users.noreply.github.com>
2025-04-20 15:25:46 +0530

    Add files via upload

---
+++ binary_tree.java
@@ -0,0 +1,27 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+public class Solution {
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorderHelper(root, result);
+        return result;
+    }
+    private static void inorderHelper(TreeNode node, List<Integer> result) {
+        if (node != null) {
+            inorderHelper(node.left, result);
+            result.add(node.val);
+            inorderHelper(node.right, result);
+        }
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(inorderTraversal(root)); 
+    }
+}
